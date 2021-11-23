71b8bd6a3e6630205a78265fc1afdfda92e9d4d6
anky2k <ankit.agarwal@fox.com>
2021-11-23 18:12:44 +0530

    add shimmer component

---
+++ components/Card/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="isLoaded">
+    <CardComp      
+      :item="item"
+    />
+  </div>  
+  <div v-else>
+    <CardLoader />
+  </div>  
+</template>
+
+<script>
+import CardLoader from './Loader.vue'
+import CardComp from './CardComp.vue'
+
+export default {
+  name: 'Card',
+  props: {
+    item: {
+      type: Object,
+      default: () => ({}),
+    },
+    isLoaded: {
+      type: Boolean,
+      default: true,
+    },
+  },
+}
+</script>
---
+++ pages/skeleton-loading.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="flex flex-wrap justify-around p-4 lg:p-16">
+    <Card
+      v-for="(item, i) in items"
+      :key="i"
+      :item="item"
+      :is-loaded="isLoaded"
+    />
+  </div>
+</template>
+
+<script>
+import Card from '../components/Card'
+
+export default {
+    name: 'SkeletonLoading',
+    components: {
+        Card,
+    },
+    data() {
+      return {
+        isLoaded: false,
+        items: [
+          {
+            thumbnail: 'https://picsum.photos/320/200',
+            avatar: 'https://picsum.photos/50/50',
+            bgColor: '#BCD1FF',
+            tag: 'PRODUCTIVITY',
+            date: '3 days ago',
+            title: '7 Skills of Highly Effective Programmers',
+            desc: 'Our team was inspired by the seven skills of highly effective programmers created by the TechLead. We wanted to provide our own take on the topic. Here are our seven...',
+            author: 'Glen Williams',
+          },
+        ],
+      }
+    },
+    mounted() {
+      this.onLoad()
+    },
+    methods: {
+        onLoad() {
+          this.isLoaded = false
+          setTimeout(() => {
+              this.isLoaded = true
+          }, 3000)
+        },
+    },
+}
+</script>
