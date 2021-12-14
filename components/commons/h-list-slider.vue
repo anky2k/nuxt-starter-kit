870a255606ad2d2757dd3e689120fa9cc32e4c8e
anky2k <ankit.agarwal@fox.com>
2021-12-14 16:46:01 +0530

    add functionality to chevron arrows

--- components/commons/h-list-slider.vue
+++ components/commons/h-list-slider.vue
@@ -1,12 +1,13 @@
 <template>  
-  <div class="rail">
-    <InView @triggerIntersected="toggleLeftChevron">
+  <div ref="slider" class="rail">
+    <InView @triggerIntersected="hideLeftArrow">
         <template v-slot:LazyLoad>
             <div />                
         </template>                    
     </InView>            
     <Left
-      ref="goLeft" 
+      v-on:click="moveLeft"
+      ref="left" 
       class="hidden z-10 lg:block w-8 md:h-8 left-56 fixed top-32 stroke-white transform hover:scale-125 transition duration-500 ease-in-out cursor-pointer"
      />
     <a
@@ -23,8 +24,15 @@
       </slot>      
     </a>    
     <Right
+      ref="right"
+      v-on:click="moveRight"
       class="hidden z-10 lg:block w-8 md:h-8 right-8 fixed top-32 stroke-white transform hover:scale-125 transition duration-500 ease-in-out cursor-pointer"
      />
+     <InView @triggerIntersected="hideRightArrow">
+        <template v-slot:LazyLoad>
+            <div />                
+        </template>                    
+    </InView>            
   </div>    
 </template>
 
@@ -47,10 +55,29 @@ export default {
     InView
   },
   methods: {
-    toggleLeftChevron(e) {
+    hideLeftArrow(e) {
       if(e.isIntersecting) {
-        this.$refs['goLeft'].style.display = 'none'
+        this.$refs['left'].style.display = 'none'
       }
+    },
+    hideRightArrow(e) {
+      if(e.isIntersecting) {
+        this.$refs['right'].style.display = 'none'
+      }
+    },
+    moveRight(e) {
+      this.$refs['slider'].scrollBy({
+        left: 800,
+        behavior: 'smooth',
+      })
+      this.$refs['left'].style.display = 'block'
+    },
+    moveLeft(e) {
+      this.$refs['slider'].scrollBy({
+        left: -800,
+        behavior: 'smooth',
+      })
+      this.$refs['right'].style.display = 'block'
     }
   }
 }
