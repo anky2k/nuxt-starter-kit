46d3280c8da91a44873d4be539392063bb2f6c7e
anky2k <ankit.agarwal@fox.com>
2021-12-14 16:51:51 +0530

    show sample to server to client transition in data

--- components/labels/cards/secondary.vue
+++ components/labels/cards/secondary.vue
@@ -1,7 +1,7 @@
 <template>  
   <Static>
     <template v-slot:StaticContent> 
-        <span class="text-gray-400 text-xs md:text-base lg:text-base">{{text}}</span>
+        <span class="text-gray-400 text-xs md:text-base lg:text-base">{{renderText()}}</span>
     </template>  
   </Static>        
 </template>
@@ -19,6 +19,16 @@ export default {
   },
   components: {
     Static
-  }  
+  },
+  methods: {
+    renderText() {            
+      if(process.server) {
+        return '...'
+      }
+      if(process.client) {
+        return this.$props.text
+      }
+    }
+  }
 }
 </script>
